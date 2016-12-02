9754931a00d766dec2d00de3b80e28f24ccd5f14
Saeed Abdullah <sma249@cornell.edu>
2016-12-02 17:15:18 -0500

    More flexible pandas requirement

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pandas==0.18.1',
+        'pandas>=0.18.1',
         'python-geohash==0.8.5',
         'geopy==1.11.0',
         'Anvil==0.1.0'
