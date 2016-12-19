c0232c1e6b2352f95292b1242e123c524b4a56bf
Xian Zhang <xz278@cornell.edu>
2016-12-19 15:29:19 -0500

    adds compute_gyration and its test

--- location/utils.py
+++ location/utils.py
@@ -99,3 +99,81 @@ def compute_total_gyration(data, sr_col='stay_region'):
         temp_sum += loc_cnt[loc] * vincenty(geohash.decode(loc), r_cm).m ** 2
 
     return math.sqrt(1/N * temp_sum)
+
+
+def compute_gyration(data,
+                     sr_col='stay_region',
+                     k=None,
+                     remove_dist_pt=True,
+                     home=None,
+                     dist_pt_th=50000):
+    """
+    Compute radius of gyration.
+
+    Parameters:
+    -----------
+
+    data: DataFrame
+        Location data.
+
+    sr_col: str
+        Column name for stay region.
+        Default is 'stay_region'.
+
+    k: int
+        k-th radius of gyration.
+        Default is None, in this case return total radius of gyration.
+        k-th radius of gyration is the radius gyration compuated up to
+        the k-th most frequent visited locations.
+
+    remove_dist_pt: bool
+        Whether remove points that are far away from home location.
+        Default is True, that is do remove far away points.
+
+    home: str
+        Geohash string for home location.
+        Default is None. In this case, approximate home location from
+        given location data.
+
+    dist_pt_th: int
+        Threshold in meter to filter out distant points.
+
+    Returns:
+    (radius of gyration): float
+        Radius of gyration in meters.
+        Return np.nan is k is greater than the number of different
+        visited locations.
+    """
+    loc_data = data.copy()
+    loc_data = loc_data[pd.notnull(loc_data[sr_col])]
+
+    visited_locs = np.unique(loc_data[sr_col])
+
+    # remove points that are not in the same city
+    if remove_dist_pt:
+        if home is None:
+            home = approx_home_location(loc_data)
+        home_coord = geohash.decode(home)
+        visited_locs_filtered = []
+        for x in visited_locs:
+            dist = vincenty(home_coord, geohash.decode(x)).m
+            if dist <= dist_pt_th:
+                visited_locs_filtered.append(x)
+        loc_data = loc_data.loc[loc_data[sr_col].isin(visited_locs_filtered)]
+
+    # compute gyration of radius
+    if k is None:
+        return compute_total_gyration(loc_data, sr_col=sr_col)
+    else:
+        cnt_locs = Counter(loc_data[sr_col])
+        # number of different visited locations
+        num_visited_locations = len(cnt_locs)
+        if k > num_visited_locations:
+            return np.nan
+        else:
+            # k most frequent visited locations
+            k_locations = cnt_locs.most_common()[:k]
+            k_locations = [x[0] for x in k_locations]
+            # compute gyration for the k most frequent locations
+            loc_data = loc_data.loc[loc_data[sr_col].isin(k_locations)]
+            return compute_total_gyration(loc_data, sr_col=sr_col)
