212fca43a1a30c19dd0b86c0dd4ea26297c0e2b7
Xian Zhang <xz278@cornell.edu>
2017-05-01 15:53:11 -0400

    Add dispalceemnt() and its test
    This function computes the list of travel distance from one
    locatio to another.

--- location/test/location_features_test.py
+++ location/test/location_features_test.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import pytest
 import location.location_features as lf
+from geopy.distance import vincenty
 
 
 def test_gyrationradius():
@@ -184,3 +185,62 @@ def test_num_clusters():
                       columns=['cluster'])
     n = lf.num_clusters(df)
     assert n == 4
+
+
+def test_displacement():
+    df = pd.DataFrame(columns=['cluster'])
+    assert len(lf.displacement(df)) == 0
+
+    df['cluster'] = [np.nan, np.nan, 'dr5xejs', 'dr5xejs']
+    df['latitude'] = [40.749562, 40.749563, 40.724195, 40.724248]
+    df['longitude'] = [-73.710272, -73.706997, -73.690573, -73.690747]
+    assert len(lf.displacement(df)) == 0
+
+    df = pd.DataFrame(columns=['cluster'])
+    df['cluster'] = [np.nan, np.nan,
+                     'dr5xejs', 'dr5xejs',
+                     np.nan, 'dr5xef2',
+                     'dr5xef2']
+    df['latitude'] = [40.749562, 40.749563,
+                      40.724269, 40.724269,
+                      40.724269, 40.706522,
+                      40.706522]
+    df['longitude'] = [-73.710272, -73.706997,
+                       -73.690737, -73.690737,
+                       -73.690737, -73.662139,
+                       -73.662139]
+    displace = lf.displacement(df)
+    assert len(displace) == 1
+    assert displace[0] == pytest.approx(3118.1779973248804, 0.0001)
+
+    df = pd.DataFrame(columns=['cluster'])
+    df['cluster'] = [np.nan, np.nan,
+                     'dr5xejs', 'dr5xejs',
+                     np.nan, 'dr5xef2',
+                     'dr5xef2', 'dr5xejs']
+    df['latitude'] = [40.749562, 40.749563,
+                      40.724269, 40.724269,
+                      40.724269, 40.706522,
+                      40.706522, 40.724048]
+    df['longitude'] = [-73.710272, -73.706997,
+                       -73.690737, -73.690737,
+                       -73.690737, -73.662139,
+                       -73.662139, -73.690753]
+    displace = lf.displacement(df)
+    assert len(displace) == 2
+    assert displace[0] == pytest.approx(3118.1779973248804, 0.0001)
+    assert displace[1] == pytest.approx(3103.7813441942367, 0.0001)
+
+    df = pd.DataFrame(columns=['cluster'])
+    df['cluster'] = [np.nan, np.nan,
+                     'dr5xejs', 'dr5xejs',
+                     np.nan, 'dr5xef2',
+                     'dr5xef2', 'dr5xejs']
+    cluster_mapping = {'dr5xejs': geohash.decode('dr5xejs'),
+                       'dr5xef2': geohash.decode('dr5xef2')}
+    displace = lf.displacement(df, cluster_mapping=cluster_mapping)
+    assert len(displace) == 2
+    expected = vincenty(cluster_mapping['dr5xejs'],
+                        cluster_mapping['dr5xef2']).m
+    assert displace[0] == pytest.approx(expected, 0.0001)
+    assert displace[1] == pytest.approx(expected, 0.0001)
